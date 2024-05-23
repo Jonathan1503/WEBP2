6b73b3178f25f9a5698f187fabac536d5eb904a2
Jonathan Rivera <j.riverao@uniandes.edu.co>
2024-05-23 10:50:59 -0500

    final

--- src/propuesta/propuesta.service.ts
+++ src/propuesta/propuesta.service.ts
@@ -1,4 +1,29 @@
+/* eslint-disable prettier/prettier */ 
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { BusinessError,BusinessLogicException } from 'src/shared/errors/business-errors';
+import { PropuestaEntity } from './propuesta.entity/propuesta.entity';
 
 @Injectable()
-export class PropuestaService {}
+export class PropuestaService {
+    constructor(
+        @InjectRepository(PropuestaEntity)
+        private readonly propuestaRespository: Repository<PropuestaEntity>
+    ){}
+   
+    async findPropuestaById(id: string): Promise<PropuestaEntity> {
+        const propuesta: PropuestaEntity = await this.propuestaRespository.findOne({where: {id}} );
+        if (!propuesta)
+          throw new BusinessLogicException("El propuesta con el id proporcionado no fue encontrado", BusinessError.NOT_FOUND);
+   
+        return propuesta;
+    }
+    async crearPropuesta(propuesta: PropuestaEntity): Promise<PropuestaEntity> {
+     
+        return await this.propuestaRespository.save(propuesta);
+    }
+    
+ 
+}
